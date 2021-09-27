424715c69a904be4fa6c7926517e3f06f1106ee5
Alex <zagorodniy88@gmail.com>
2021-09-27 12:47:28 -0300

    Start JS Course

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+window.alert("Hello, World!");
+
+var A = "this is a string";
+// Changed a color
+var B = A.fontcolor('red');
+var C = 2+2;
+
+document.write(B);
+document.write("<br>"+C);
+document.write("<br>"+"I love "
++ "Js" + "very much");
